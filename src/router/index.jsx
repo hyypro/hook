0dcadccaec61f762b04cb62f5047b20133c9838c
hyypro <3126924360@qq.com>
2020-06-13 11:18:26 +0800

    'x'

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import Home from '@/pages/home'
 
 export default function Router () {
     return (
       <div>
         <BrowserRouter>
-          <Switch>
             <Route path="/" component={Home}/>
-          </Switch>
         </BrowserRouter>
       </div>
     )
